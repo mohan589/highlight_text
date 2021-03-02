0ca7b1ad735181d90769e4c4948bd426859d93bd
Mohan <mohan.pitchikala@sumtotalsystems.com>
2021-03-02 14:54:12 +0530

    highlight text

---
+++ your_highlighting_class.rb
@@ -0,0 +1,103 @@
+require 'colorize'
+
+class YourHighlightingClass
+  def initialize(content, highlights)
+    @content = content
+    @highlights = highlights
+  end
+
+  def apply_random_colors
+    result = ''
+    paragraphs.each_with_index do |item, index|
+      data = item.split(' ')
+      @highlights.each do |key, value|
+        sub_data = data[key[:start_word]..key[:end_word]]
+        colorize_data = "<span style=\"color:#{random_color};font-weight:bold\">" + sub_data.join(' ') + "</span>"  unless sub_data.nil?
+        data[key[:start_word], key[:end_word]] = colorize_data
+      end
+      result = result + "<p class=\"tooltip\">" + data.join(' ') + "</p>"
+    end
+
+    prepare_html_content { result }
+  end
+
+  def apply
+    apply_random_colors
+  end
+
+  private
+  def random_color
+    String.colors.sample
+  end
+
+  def prepare_html_content
+    content = "<!DOCTYPE html>
+    <html>
+    <head>
+    <style>
+      .tooltip {
+        position: relative;
+      }
+
+      .tooltip .tooltiptext {
+        visibility: hidden;
+        width: 120px;
+        background-color: white;
+        text-align: center;
+        border-radius: 6px;
+        padding: 5px 0;
+      
+        /* Position the tooltip */
+        position: absolute;
+        z-index: 1;
+      }
+      
+      .tooltip:hover .tooltiptext {
+        visibility: visible;
+      }
+    </style>
+    </head
+    <body>
+    #{yield}
+    </body>
+    </html>
+    "
+    html_template(content)
+  end
+
+  def paragraphs
+    @content.split(/\n\n/)
+  end
+
+  def html_template(input)
+    File.open("highlight_text.html", "w") { |f| f.write input }
+  end
+
+  attr_accessor :content, :highlights
+end
+
+content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas consectetur malesuada velit, sit amet porta magna maximus nec. Aliquam aliquet tincidunt enim vel rutrum. Ut augue lorem, rutrum et turpis in, molestie mollis nisi. Ut dapibus erat eget felis pulvinar, ac vestibulum augue bibendum. Quisque sagittis magna nisi. Sed aliquam porttitor fermentum. Nulla consequat justo eu nulla sollicitudin auctor. Sed porta enim non diam mollis, a ullamcorper dolor molestie. Nam eu ex non nisl viverra hendrerit. Donec ante augue, eleifend vel eleifend quis, laoreet volutpat ipsum. Integer viverra aliquam nulla, ac rutrum dui sodales nec.
+
+Sed turpis enim, porttitor nec maximus sed, luctus pretium elit. Sed sodales imperdiet velit, vitae viverra erat commodo non. Nunc porttitor risus sit amet quam faucibus, et luctus ex fringilla. Mauris quis urna non lacus tempor iaculis vitae quis dolor. Nam vitae pulvinar lacus, quis varius erat. Etiam lobortis orci vitae elementum tempor. Praesent convallis euismod enim vel vestibulum. Proin vitae eros vitae nisi cursus dapibus vitae at ipsum. Phasellus sed tempor eros, non scelerisque nunc. Nullam condimentum ex ultrices, ultrices ante sit amet, rhoncus nibh. Aliquam fermentum vulputate fringilla. Ut risus orci, pharetra eu tellus vel, fringilla feugiat dolor.
+
+Nunc quis elit quam. Sed aliquet, nibh ut sagittis egestas, lorem tortor laoreet diam, non maximus lectus dolor dignissim eros. Sed vehicula mi id aliquet aliquam. Vestibulum sed lacus et neque dictum convallis in vitae mauris. Etiam varius augue vel mattis tempor. Curabitur mattis facilisis metus, tempus consectetur quam aliquam sed. Mauris velit orci, efficitur sit amet nisl in, finibus dictum elit. In lectus augue, elementum eu sapien sed, auctor tincidunt urna.
+
+Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer lacinia accumsan velit. Duis vel facilisis libero. Cras consequat sit amet mauris ut ultrices. Ut pulvinar sit amet odio sit amet pretium. Nullam tortor ligula, consequat non nisl vitae, rutrum placerat est. Sed finibus interdum justo vel placerat. Cras varius tortor sed justo tempus scelerisque. Praesent facilisis ex vitae iaculis iaculis. Sed consectetur a lectus non condimentum. Etiam id lacus a nulla cursus laoreet. Vivamus ipsum purus, sodales vel metus varius, viverra mollis justo. Nulla facilisi. Vivamus volutpat nunc elit, quis sollicitudin velit ornare sit amet.
+
+Nullam fringilla nisi nunc, vitae accumsan tortor luctus quis. Sed facilisis, est ut eleifend sagittis, felis dolor pellentesque lectus, in congue purus orci non nunc. Nunc finibus eu metus et volutpat. Integer hendrerit tortor et tellus euismod vulputate. Aliquam erat volutpat. Aenean gravida justo in risus feugiat, ut suscipit tortor ullamcorper. Nam a sapien dictum, vestibulum eros vitae, sodales turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed ultricies at elit et rutrum. Sed placerat erat quis condimentum convallis. Duis ornare magna nec ante faucibus malesuada. Duis a erat sed sapien semper eleifend. Mauris consequat nibh sollicitudin mi euismod, non ultricies lectus bibendum. Cras a erat libero. Aliquam nisl ipsum, scelerisque at risus a, hendrerit vestibulum sapien. Proin luctus diam eu mi lobortis molestie id vel ante."
+
+highlights = [{
+  start_word: 20,
+  end_word: 35,
+  comment: 'Foo'
+}, {
+  start_word: 73,
+  end_word: 92,
+  comment: 'Bar'
+}, {
+  start_word: 50,
+  end_word: 98,
+  comment: 'Baz'
+}]
+
+YourHighlightingClass.new(content, highlights).apply
